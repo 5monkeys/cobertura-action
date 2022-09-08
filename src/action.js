ed49cc3dff87e8c86e0115bdc1d73b5547a6c467
Kevin Valk <kevin@codean.io>
2022-09-08 15:48:43 +0200

    Revert "Allow specifying source dir for missing line links (#59)"
    This reverts commit 47d3d6fd59efbac93529e27c71b88fa8e4c5a5e9.

--- src/action.js
+++ src/action.js
@@ -44,8 +44,6 @@ async function action(payload) {
   const linkMissingLines = JSON.parse(
     core.getInput("link_missing_lines", { required: false }) || "false"
   );
-  const linkMissingLinesSourceDir =
-    core.getInput("link_missing_lines_source_dir", { required: false }) || null;
   const onlyChangedFiles = JSON.parse(
     core.getInput("only_changed_files", { required: true })
   );
@@ -64,7 +62,6 @@ async function action(payload) {
     showMissing,
     showMissingMaxLength,
     linkMissingLines,
-    linkMissingLinesSourceDir,
     filteredFiles: changedFiles,
     reportName,
   });
@@ -88,13 +85,9 @@ async function action(payload) {
   }
 }
 
-function formatFileUrl(sourceDir, fileName, commit) {
+function formatFileUrl(fileName, commit) {
   const repo = github.context.repo;
-  sourceDir = sourceDir ? sourceDir : "";
-  // Strip leading and trailing slashes.
-  sourceDir = sourceDir.replace(/\/$/, "").replace(/^\//, "");
-  const path = (sourceDir ? `${sourceDir}/` : "") + fileName;
-  return `https://github.com/${repo.owner}/${repo.repo}/blob/${commit}/${path}`;
+  return `https://github.com/${repo.owner}/${repo.repo}/blob/${commit}/${fileName}`;
 }
 
 function formatRangeText([start, end]) {
@@ -157,7 +150,6 @@ function markdownReport(reports, commit, options) {
     showMissing = false,
     showMissingMaxLength = -1,
     linkMissingLines = false,
-    linkMissingLinesSourceDir = null,
     filteredFiles = null,
     reportName = "Coverage Report",
   } = options || {};
@@ -182,7 +174,7 @@ function markdownReport(reports, commit, options) {
         status(fileTotal),
         showMissing && file.missing
           ? formatMissingLines(
-              formatFileUrl(linkMissingLinesSourceDir, file.filename, commit),
+              formatFileUrl(file.filename, commit),
               file.missing,
               showMissingMaxLength,
               linkMissingLines
